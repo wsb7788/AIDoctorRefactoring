77b8afe684e514fb143ef36264a1043f25a9624d
wsb7788 <wsb0514@naver.com>
2021-12-04 02:44:43 +0900

    login api 통신 제외 구현

--- app/src/main/java/com/project/aidoctor/ui/login/LoginActivity.kt
+++ app/src/main/java/com/project/aidoctor/ui/login/LoginActivity.kt
@@ -32,28 +32,38 @@ class LoginActivity : BaseActivity(), LoginListener {
         binding.ivPassword.setOnClickListener(this)
         binding.ivEmail.setOnClickListener(this)
         binding.button.setOnClickListener(this)
-        //binding.btnBack.setOnClickListener(this)
+
     }
 
     private fun idObserve() {
         viewModel.email.observe(this,{
             if(viewModel.email.value.isNullOrEmpty()){
-           //     binding.ivEmail.setImageResource(R.drawable.ic_email_delete_off)
+                binding.ivEmail.setImageResource(R.drawable.ic_email_delete_off)
+                binding.clEmail.setBackgroundResource(R.drawable.login_border)
                 return@observe
             }
-         //   binding.ivEmail.setImageResource(R.drawable.ic_email_delete_on)
+            binding.ivEmail.setImageResource(R.drawable.ic_email_delete_on)
+            binding.clEmail.setBackgroundResource(R.drawable.login_border_active)
         })
     }
 
     private fun pwShowObserve() {
         viewModel.showPw.observe(this, {
             if(viewModel.showPw.value!!){
-             //   binding.ivPassword.setImageResource(R.drawable.ic_password_see_on)
+                binding.ivPassword.setImageResource(R.drawable.ic_password_see_on)
                 binding.etPassword.inputType = InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
                 return@observe
             }
-        //    binding.ivPassword.setImageResource(R.drawable.ic_password_see_off)
+            binding.ivPassword.setImageResource(R.drawable.ic_password_see_off)
             binding.etPassword.inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD or InputType.TYPE_CLASS_TEXT
+
+        })
+        viewModel.pw.observe(this,{
+            if(viewModel.pw.value.isNullOrEmpty()){
+                binding.clPassword.setBackgroundResource(R.drawable.login_border)
+                return@observe
+            }
+            binding.clPassword.setBackgroundResource(R.drawable.login_border_active)
         })
 
     }
@@ -63,7 +73,7 @@ class LoginActivity : BaseActivity(), LoginListener {
             binding.ivPassword-> viewModel.showPw()
             binding.ivEmail -> viewModel.emailBlankCheck()
             binding.button -> startMain()
-            //binding.btnBack -> onBackPressed()
+
         }
     }
 
