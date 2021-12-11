ccefdd908859c41fe6a619f961a00b6ce0cb1e66
wsb7788 <wsb0514@naver.com>
2021-12-12 00:01:52 +0900

    hospital 위치 이동 및 좌표찍기

--- app/src/main/java/com/project/aidoctor/ui/hospital/HospitalActivity.kt
+++ app/src/main/java/com/project/aidoctor/ui/hospital/HospitalActivity.kt
@@ -1,23 +1,17 @@
 package com.project.aidoctor.ui.hospital
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.MenuItem
 import android.view.View
 import androidx.databinding.DataBindingUtil
-import com.google.android.material.navigation.NavigationBarView
 import com.project.aidoctor.R
-import com.project.aidoctor.databinding.ActivityChatBinding
 import com.project.aidoctor.databinding.ActivityHospitalBinding
-import com.project.aidoctor.databinding.ActivityMainBinding
 import com.project.aidoctor.ui.BaseActivity
-import com.project.aidoctor.ui.home.HomeFragment
 import com.project.aidoctor.ui.home.HospitalModel
-import com.project.aidoctor.ui.profile.ProfileFragment
+import net.daum.mf.map.api.MapPOIItem
 import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
-
 import org.koin.androidx.viewmodel.ext.android.viewModel
+
 
 class HospitalActivity : BaseActivity() {
 
@@ -47,9 +41,23 @@ class HospitalActivity : BaseActivity() {
         else
             binding.tvUrl.text = item.url
 
+        val xpo = item.xPos
+        val ypo = xpo!!.toDouble()
         val mapView = MapView(this)
         binding.mv.addView(mapView)
-        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.53737528, 127.00557633), true);
+        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(item.yPos!!.toDouble(),item.xPos!!.toDouble() ), true);
+
+        val marker = MapPOIItem()
+        marker.itemName = item.name
+        marker.tag = 0
+        marker.mapPoint = MapPoint.mapPointWithGeoCoord(item.yPos!!.toDouble(), item.xPos!!.toDouble())
+        marker.markerType = MapPOIItem.MarkerType.BluePin // 기본으로 제공하는 BluePin 마커 모양.
+
+        marker.selectedMarkerType =
+            MapPOIItem.MarkerType.RedPin // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+
+
+        mapView.addPOIItem(marker)
     }
 
     override fun onClick(v: View?) {
