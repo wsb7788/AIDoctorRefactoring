2c3033c698e93d577a14b31bd4459de34e1bbbe5
wsb7788 <wsb0514@naver.com>
2021-12-15 05:26:28 +0900

    admin emList time format 지정

--- app/src/main/java/com/project/aidoctor/ui/admin/EmergencyViewHolder.kt
+++ app/src/main/java/com/project/aidoctor/ui/admin/EmergencyViewHolder.kt
@@ -8,11 +8,18 @@ import com.project.aidoctor.data.remote.admin.EmUser
 import com.project.aidoctor.data.remote.chat.EmergencyResponse
 import com.project.aidoctor.databinding.LayoutRecyclerEmListBinding
 import com.project.aidoctor.databinding.LayoutRecyclerFileBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class EmergencyViewHolder(val binding: LayoutRecyclerEmListBinding): RecyclerView.ViewHolder(binding.root) {
 
     fun bind(emUser:EmUser){
         binding.tvName.text = emUser.USER_USERID
         Glide.with(ApplicationClass.instance).load(R.drawable.no_profile).into(binding.iv)
+        val time = Date(System.currentTimeMillis())
+        val a = SimpleDateFormat("aa hh:mm")
+        val aa  = a.format(time)
+        binding.tvTime.text= aa
+
     }
 }
